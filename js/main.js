0ce8da9546ee256c0f0a093fb6e1a7cf9eb15eae
Khoi Nguyen <43136539+akhoinguyen@users.noreply.github.com>
2020-02-11 12:52:46 -0500

    Completed img carousel using my solution by assigning each img an index variable then accordingly show/hide them using js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var imgindex = 0;
+
+$(`img:eq(0)`).removeClass("hidden");
+
+$(".next").on('click', function(e) {
+
+$(`img:eq(${imgindex})`).addClass("hidden");
+imgindex++;
+if(imgindex > 2) {
+  imgindex = 0;
+}
+$(`img:eq(${imgindex})`).removeClass("hidden");
+})
+
+$(".prev").on('click', function(e) {
+
+$(`img:eq(${imgindex})`).addClass("hidden");
+imgindex--;
+if(imgindex < -2) {
+  imgindex = 0;
+}
+$(`img:eq(${imgindex})`).removeClass("hidden");
+})
+
+/* how it works: assign each img a var: img-index (first one starts at 0). WHen Next is clicked, hide current img, increase img-index by 1 and show the img with the new img-index */
